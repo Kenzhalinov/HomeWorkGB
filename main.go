44bb471ae197a5e92f9c1c8bd3e8555533134cfd
Maksat Kenzhalinov <mkenzhalinov@gmail.com>
2024-04-08 09:54:46 +0500

    first version

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	arr := [5]string{"1", "2", "3", "4", "5"}
+	fmt.Println("Введите элементы массива через пробел:")
+	_, _ = fmt.Scan(&arr)
+
+	randomInt := rand.Intn(3) + 1
+	// newArr := make([]string, len(arr)
+	result := make([]string, 0, randomInt)
+
+	for i := 0; i < randomInt && i < len(arr); i++ {
+		result = append(result, arr[i])
+	}
+	fmt.Println(result)
 }
